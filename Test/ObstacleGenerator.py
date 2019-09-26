d3d957877c66c1f6bfc5480645d8da6b83af81d1
Jacob Schif <jacob.robert.005@gmail.com>
2019-09-26 18:39:30 -0500

    added ObstacleGenerator

---
+++ Test/ObstacleGenerator.py
@@ -0,0 +1,19 @@
+import random
+
+
+class ObstacleGenerator:
+
+    def __init__(self):
+
+        self.lengths = [12, 12, 24, 24, 36]
+        self.maxx = 96
+        self.maxy = 72
+        self.points = []
+
+    def generate(self):
+
+        check = False
+
+        while (check == False):
+
+            for length in self.lengths:
