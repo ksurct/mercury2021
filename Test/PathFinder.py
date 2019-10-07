d1876279f9b54b66524c20c07b25ef064ccad347
Caden Churchman <ccaden21@gmail.com>
2019-10-07 18:50:20 -0500

    Made it sort of work

--- Test/PathFinder.py
+++ Test/PathFinder.py
@@ -1,120 +1,175 @@
-
+from time import sleep
 class PathFinder(object):
     """docstring for PathFinder."""
 
     def __init__(self, ObstacleField):
         self.field = ObstacleField # expects big array with all valid points 0 and not valid 1 or True False
         self.point = False # its false not a point
-        self.xMax = 95
-        self.yMax = 71
         self.width = 24
+        self.debug = True
 
-    def checkField():
-        self.point = findStart()
+    def setField(self, field):
+        self.field = field
+        self.xMax = len(self.field) - 1
+        self.yMax = len(self.field[0]) - 1
+
+    def findStart(self):
+        for x in range(0, len(self.field[0])):
+            if self.isValid([0,x]):
+                return [0,x]
+        return False
+
+    def checkField(self):
+        self.point = self.findStart()
         if (not self.point):
+            print('no point found')
             return False # it didnt even find a findStart
         # do the 'algo rythem' now
-        return moveForeward()
+        self.setPoint(self.point)
+        return self.moveForeward()
 
-    def moveForeward(point):
+    def moveForeward(self):
+        print('check field')
         stack = [];
-        stack.append(point)
-        while len(stack) > 0:
+        while True:
 
-            if (not moveForwardIsValid(self.point)):
+            if (not self.moveForwardIsValid(self.point)):
+                print(self.point)
                 curPoint = self.point
                 stack.append(self.point)
                 overideDown = False
-                if (not downForward() and not overideDown):
+                if (not overideDown and not self.downForward()):
                     self.point = curPoint # get the last valid foreward value
-                    if (not upForward() and len(stack) == 0):
+                    if (not self.upForward() and len(stack) == 0):
                         return False
                     elif (len(stack) > 0):
                         self.point = stack.pop() # if can't move up foreward and we have a stack make the self.point equal the last valid foreward point
                         overideDown = True # the only reason we
                 else:
-                    if (not upForward() and len(stack) == 0):
+                    if (not self.upForward() and len(stack) == 0):
                         return False
-                    elif (len(stack) > 0):
+                    elif (len(sFalsetack) > 0):
                         self.point = stack.pop()
                     overideDown = False
-            self.point = [self.point[0] + 1, self.point[1]] # just keep going
-    def downForward():
+            print("Went Forward")
+            self.setPoint([self.point[0] + 1, self.point[1]]) # just keep going
+
+    def downForward(self):
+        print('down FORWARD')
         while True:
-            if (moveForwardIsValid(self.point)):
+            if (self.moveForwardIsValid(self.point)):
                 return True
-            if (not moveDownIsValid(self.point)):
-                return backDown(self.point)
-        self.point = [self.point[0], self.point[1] + 1]
+            if (not self.moveDownIsValid(self.point)):
+                return self.backDown()
+            self.setPoint([self.point[0], self.point[1] + 1])
 
-    def backDown():
+    def backDown(self):
         while True:
-            if (not moveBackwardIsValid()):
-                return upBack()
-            if (moveDownIsValid()):
-                return downForward()
-            self.point = [self.point[0] - 1, self.point[1]]
+            if (self.moveDownIsValid(self.point)):
+                return self.downForward()
+            if (not self.moveBackwardIsValid(self.point)):
+                return self.upBack()
+            self.setPoint([self.point[0] - 1, self.point[1]])
 
-    def upBack():
+    def upBack(self):
         while True:
-            if (not moveUpIsValid(self.point)):
+            if (not self.moveUpIsValid(self.point)):
                 return False
-            if (moveBackwardIsValid()):
-                return backDown()
-            self.point = [self.point[0], self.point[1] - 1];
+            if (self.moveBackwardIsValid(self.point)):
+                return self.backDown()
+            self.setPoint([self.point[0], self.point[1] - 1])
 
-    def upForward():
+    def upForward(self):
         while True:
-            if (moveForwardIsValid(self.point)):
+            if (self.moveForwardIsValid(self.point)):
                 return True
-            if (not moveUpIsValid(self.point)):
-                return backUp()
-        self.point = [self.point[0], self.point[1] - 1]
+            if (not self.moveUpIsValid(self.point)):
+                return self.backUp()
+        self.setPoint([self.point[0], self.point[1] - 1])
 
-    def backUp():
+    def backUp(self):
         while True:
-            if (moveUpIsValid(self.point)):
-                return upForward()
-            if (not moveBackwardIsValid(self.point)):
-                return downBack()
-            self.point = [self.point[0] - 1, self.point[1]]
+            if (self.moveUpIsValid(self.point)):
+                return self.upForward()
+            if (not self.moveBackwardIsValid(self.point)):
+                return self.downBack()
+            self.setPoint([self.point[0] - 1, self.point[1]])
 
-    def downBack():
+    def downBack(self):
         while True:
-            if (moveBackwardIsValid(self.point)):
+            if (self.moveBackwardIsValid(self.point)):
                 return backUp()
-            if (not moveDownIsValid()):
+            if (not self.moveDownIsValid(self.point)):
                 return False
-            self.point = [self.point[0], self.point[1] - 1]
+            self.setPoint([self.point[0], self.point[1] + 1])
+    """
+    Might need to implement forwardUp and forwardDown
+    """
 
 
 
-    def isValid(point): # i have defined the field to be field[x][y] not field[y][x]
+    def isValid(self, point): # i have defined the field to be field[x][y] not field[y][x]
         for x in range(0, self.width):
-            if (protectedCheckPoint(point[0] + x, point[1]) or protectedCheckPoint(point[0] + x, point[1] + self.width) or protectedCheckPoint(point[0], point[1] + x) or protectedChecksPoint(point[0] + self.width, point[1] + x)):
+            if (self.protectedCheckPoint(point[0] + x, point[1]) or self.protectedCheckPoint(point[0] + x, point[1] + self.width - 1) or self.protectedCheckPoint(point[0], point[1] + x) or self.protectedCheckPoint(point[0] + self.width - 1, point[1] + x)):
+                if (point[0] < 0):
+                    print("its negative")
                 return False;
+        return True
 
-    def protectedCheckPoint(x, y):
-        if (x > self.xMax): # we will allow points outside the x direction to be valid
+    def protectedCheckPoint(self, x, y):
+        if (y > self.yMax or y < 0 or x < 0 or x > self.xMax): # we will not allow points outside the y zone to be valid
             return True;
-        elif (y > self.yMax or y < 0): # we will not allow points outside the y zone to be valid
-            return False;
-        return self.field[x][y]; # just check the point now
+        return self.field[x][y] # just check the point now
 
-    def moveDownIsValid(point):
-        return isValid([point[0], point[1] + 1])
+    def moveDownIsValid(self, point):
+        return self.isValid([point[0], point[1] + 1])
 
-    def moveUpIsValid(point):
-        return isValid([point[0], point[1] - 1])
+    def moveUpIsValid(self, point):
+        return self.isValid([point[0], point[1] - 1])
 
-    def moveForwardIsValid(point):
-        return isValid([point[0] + 1, point[1]])
+    def moveForwardIsValid(self, point):
+        return self.isValid([point[0] + 1, point[1]])
 
-    def moveBackwardIsValid(point):
-        return isValid([point[0] - 1, point[1]])
+    def moveBackwardIsValid(self, point):
+        return self.isValid([point[0] - 1, point[1]])
 
-    def findStart():
-        for x in range(0, len(self.field[0])):
-            if isValid(x, 0):
-                return [x,0]
-        return False
+    def printField(self, field):
+        for y in range(0, 72):
+            print('end')
+            for x in range(0, 94):
+                if (field[x][y]):
+                    print(' x', end ="")
+                else:
+                    print(' 0', end ="")
+                # print(field[x][y], end = "")
+
+    def setPoint(self, point):
+        self.point = [point[0], point[1]]
+        if (self.debug):
+            print('\n\n\n\n\n\n\n')
+            print(self.point)
+            for y in range(0, self.yMax):
+                print("")
+                for x in range(0, self.xMax):
+                    if (x > point[0] and x < point[0] + 24 and y > point[1] and y < point[1] + 24):
+                        print(" *", end = "")
+                    elif (self.field[x][y]):
+                        print(" x", end = "")
+                    else:
+                        print(" 0", end = "")
+            sleep(0.01)
+    def createDummyField(self):
+        field = [[0 for i in range(72)] for j in range(94)]
+        for x in range(0, len(field)):
+            for y in range(0, len(field[0])):
+                if (x == 67 or y == 56):
+                    field[x][y] = 1
+                else:
+                    field[x][y] = 0
+        return field
+
+finder = PathFinder(1)
+finder.printField(finder.createDummyField())
+finder.setField(finder.createDummyField())
+# print(finder.protectedCheckPoint(-1, 0))
+field = finder.checkField()
