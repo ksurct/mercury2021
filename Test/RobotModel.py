ab667533be0564e9694a1249db3725a732c8dda9
Caden Churchman <ccaden21@gmail.com>
2019-11-04 14:25:58 -0600

    This is the start of the robot model

--- Test/RobotModel.py
+++ Test/RobotModel.py
@@ -1,1 +1,21 @@
-This is the start of the model
+
+
+
+
+
+
+
+
+class RobotModel:
+
+    def __init__(self, type):
+        self.type = type
+
+    def moveFK(self, vOne, vTwo):
+        print("moved")
+
+    def moveTheta(radians):
+        print("Did the rotation")
+
+    def moveDistance(meters):
+        print("meters have been moved")
