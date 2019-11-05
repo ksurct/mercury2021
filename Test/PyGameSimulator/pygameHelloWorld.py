38a19aad3258eeb095f9ac84cf8d2cee3f767a27
J. Isaiah Garrett <jisaiahgarrett@gmail.com>
2019-11-04 19:18:42 -0600

    Added starting code for simulator in pygame

---
+++ Test/PyGameSimulator/pygameHelloWorld.py
@@ -0,0 +1,34 @@
+# Note, this file must be run in a bash shell with pygame installed with pip
+import pygame
+
+def main():
+
+    
+    (nPass, nFail) = pygame.init()
+    print(nPass)
+    print(nFail)
+
+    # load and set the logo
+    #logo = pygame.image.load("logo32x32.png")
+    #pygame.display.set_icon(logo)
+    pygame.display.set_caption("minimal program")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((240,180))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+
+
+if __name__=="__main__":
+    main()
