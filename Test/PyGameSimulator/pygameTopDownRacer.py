2e04990458870862b2fc0fe0e87abfd991972119
Jacob Schif <jacob.robert.005@gmail.com>
2019-11-18 19:21:58 -0600

    made oppy transparent

--- Test/PyGameSimulator/pygameTopDownRacer.py
+++ Test/PyGameSimulator/pygameTopDownRacer.py
@@ -21,7 +21,7 @@ gameDisplay = pygame.display.set_mode((display_width, display_height)) # This is
 pygame.display.set_caption(title)
 clock = pygame.time.Clock() # Our game clock
 
-carImg = pygame.image.load('SimpleOppy.png') # TODO use gimp to make this transparent
+carImg = pygame.image.load('SimpleOppy.png') # I made this transparent
 
 def car(x,y):
     gameDisplay.blit(carImg, (x,y)) # Blit draws the car
@@ -37,7 +37,7 @@ def game_loop():
     gameExit = False # Reset on car crashes
 
     while not gameExit:
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameExit = True
@@ -50,22 +50,22 @@ def game_loop():
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
 
-            
+
 
 
             print(event)
         x += x_change # apply change to our car
 
         gameDisplay.fill(white)
-        car(x,y) # Draw car after fill, Isaiah you dummy. 
+        car(x,y) # Draw car after fill, Isaiah you dummy.
 
         # Logic loop to check for things like collisions
         if x > display_width - car_width or x < 0:
             gameExit = True
 
 
-        pygame.display.update() # or pygame.display.flip(). Updates the frame. 
-        
+        pygame.display.update() # or pygame.display.flip(). Updates the frame.
+
         clock.tick(fps) # This argument is the frames per second
 
 game_loop()
