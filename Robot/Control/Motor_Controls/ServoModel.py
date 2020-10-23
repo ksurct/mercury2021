0a5d6ab0fb8616518ec0b07917672f665c8253df
Jacob Schif <jacob.robert.005@gmail.com>
2020-10-23 14:25:50 -0500

    cleaned things up a bit

--- Robot/Control/Motor_Controls/ServoModel.py
+++ Robot/Control/Motor_Controls/ServoModel.py
@@ -1,67 +1,46 @@
-'''
-import RPi.GPIO as GPIO
-import busio
-try:
-    import adafruit_pca9685
-except:
-    pass
-from time import sleep
-import board
-
-i2c_bus = busio.I2C(board.SCL,board.SDA)
-#GPIO.setmode(GPIO.BOARD)
-'''
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels = 8)
 
+import adafruit_motor.servo
+
+#   sudo pip3 install adafruit-circuitpython-pca9685
+#   sudo pip3 install adafruit-circuitpython-servokit
+
+
 class ServoModel():
-
-    def __init__(self,name,channel,minPWM,maxPWM):
-        #self.pwm = adafruit_pca9685.PCA9685(i2c_bus)
-        #self.pwm.frequency = 60
-        
-        self.servo = kit.servo[channel]
-        
-        self.name = name
-        
-        
-        #self.channel = channel
-        #self.presetDict = presetDict
-        self.minPWM = minPWM
-        self.maxPWM = maxPWM
-
-        #self.currentPWM = 100
-        #self.currentAngle = self.calcAnglefromPWM(self.currentPWM)
-
-        #self.pwm.set_pwm(self.channel, 0, self.currentPWM)
     '''
-    def calcAnglefromPWM(self,PWM):
-        return (90*(PWM - self.minPWM)/(self.maxPWM - self.minPWM))
+    Class to contain servo model and presets and to move servo.
+    '''
 
-    def calcPWMfromAngle(self,angle):
-        return int((angle/90)*(self.maxPWM-self.minPWM)+self.minPWM)
+    def __init__(self,name,channel,presetDict={'home':0},minAngle=0,maxAngle=180):
+        #self.servo = kit.servo[channel]
+        self.servo = adafruit_motor.servo.Servo(channel)
+        self.name = name
+        self.presetDict = presetDict
+        self.minAngle = minAngle
+        self.maxAngle = maxAngle
 
-    def updatePWM(self,PWM):
-        PWM = int(PWM)
-        if (PWM > self.maxPWM):
-            PWM = self.maxPWM
-        elif (PWM < self.minPWM):
-            PWM = self.minPWM
-        self.currentPWM = PWM
-        self.currentAngle = self.calcAnglefromPWM(self.currentPWM)'''
-
-    def updateRelativeAngle(self,angle):
-        finalAngle = self.currentAngle + angle
-        finalPWM = self.calcPWMfromAngle(finalAngle)
-        self.updatePWM(finalPWM)
+        self.servo.actuation_angle = maxAngle   #set max angle in the kit.servo controller
     
     def updateAbsoluteAngle(self,angle):
-        '''finalPWM = self.calcPWMfromAngle(angle)
-        self.updatePWM(finalPWM)'''
+        #Ensure angle remains in the range
+        if angle < self.minAngle:
+            angle = self.minAngle
+        elif angle > self.maxAngle:
+            angle = self.maxAngle
+
+        #Set angle on physical servo
         self.servo.angle = angle
 
+    def updateRelativeAngle(self,angle):
+        self.updateAbsoluteAngle(self.currentAngle + angle)
+
+    def moveToPreset(self,presetKey):
+        self.updateAbsoluteAngle(self.presetDict[presetKey])
+
+
 if __name__ == "__main__":
-    testServo = ServoModel('testServo',0,0,250)
+    testServo = ServoModel('testServo',0)
 
     userinput = 100
     print('Enter Angle(enter -1 to exit)')
