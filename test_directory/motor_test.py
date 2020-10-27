1f1087db72f4bcb15652fa54bfbd24805c6de21a
Reece Berens <rberens@ksu.edu>
2020-10-26 19:39:03 -0500

    Romote keyboard

--- test_directory/motor_test.py
+++ test_directory/motor_test.py
@@ -11,26 +11,29 @@ class motor():
         self.PWMpin1 = GPIO.PWM(PWMpin, 1000)
         self.PWMpin1.ChangeDutyCycle(100)
         self.PWMpin1.start(0)
+        self.DIRpin = DIRpin
     
     def setSpeed(self, speed):
         self.speed = speed
         self.PWMpin1.ChangeDutyCycle(speed)
     
-    def direction(self, FORWARD, BACK):
+    def setDirection(self, FORWARD, BACK):
         if(FORWARD == True):
             self.dir = self.FORWARD
         else:
             self.dir = self.BACK
+        GPIO.output(self.DIRpin, GPIO.LOW if self.dir == self.FORWARD else GPIO.HIGH)
             
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-M1 = motor(18, 24)
-M2 = motor(12, 7)
-M3 = motor(13, 5)
-M4 = motor(26, 6)
+if (__name__ == "__main__"):
+    M1 = motor(18, 24)
+    M2 = motor(12, 7)
+    M3 = motor(13, 5)
+    M4 = motor(26, 6)
 
-M1.setSpeed(0)
-M2.setSpeed(0)
-M3.setSpeed(0)
-M4.setSpeed(0)
+    M1.setSpeed(0)
+    M2.setSpeed(0)
+    M3.setSpeed(0)
+    M4.setSpeed(0)
