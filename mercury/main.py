c31ef87cd64aff5e90b1f7e28f834ea973faa4d9
Caden Churchman <ccaden21@gmail.com>
2021-01-29 14:17:06 -0600

    Added general case arguments system with needed classes.

--- mercury/main.py
+++ mercury/main.py
@@ -1,1 +1,8 @@
+from mercury.settings.settings import settings
+import sys
 print("This module is working.")
+
+settings.takeArgs(sys.argv)
+
+for setting in settings.getAll():
+    print(setting.value)
--- mercury/settings/settings.py
+++ mercury/settings/settings.py
@@ -2,21 +2,39 @@
 
 import sys
 
-settings = {
-    'instance': {
-        'value': 'realRobot',
-        'valid': {'realRobot','simRobot', 'driver'},
-    },
-}
+class AllSettings():
+    def __init__(self, settings):
+        self.settings = settings
 
-def args(args):
-    for i in range(0, len(args)):
-        if (args[i].find("-") != -1 and args[i].replace('-', '') in settings): # makes sure the args are valid settings
-            if (i < len(args) - 1): # if there is another argument for the setting
-                if (True or args[i + 1] in settings[args[i].replace('-', '')]['valid']): # if 
-                    settings[args[i].replace('-', '')]['value'] = args[i+1]
-                else:
-                    print("{} was an invalid argument for {}".format(args[i+1], args[i]))
-                    print("Keeping original value....")
-# run the argument program
-args(sys.argv)
+    def getAll(self):
+        return self.settings
+
+    def get(self, name):
+        for s in self.settings:
+            if (s.name == name):
+                return s
+        return None
+
+    def takeArgs(self, args):
+        for i in range(0, len(args)):
+            if (args[i].find("-") != -1 and settings.get(args[i].replace('-', '')) != None): # makes sure the args are valid settings
+                if (i < len(args) - 1): # if there is another argument for the setting
+                    settings.get(args[i].replace('-', '')).setVal(args[i+1])
+
+class Setting():
+    def __init__(self, name, defautVal, possibleVals):
+        self.value = defautVal
+        self.name = name
+        self.possibleVals = possibleVals
+
+    def setVal(self, newVal):
+        if (newVal in self.possibleVals):
+            self.value = newVal
+        else:
+            print("Invalid value '{}' for '{}'".format(newVal, self.name))
+            print("Keeping original value: '{}'".format(self.value))
+
+
+settings = AllSettings([
+    Setting("instance", 'realbot', ['realbot', 'simbot', 'driver']),
+])
