082f2929fb5fd8568eab2deb1e4092b44d0948bf
Caden Churchman <ccaden21@gmail.com>
2021-01-29 14:58:24 -0600

    Updated argument tool

--- mercury/settings/settings.py
+++ mercury/settings/settings.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class AllSettings():
     def __init__(self, settings):
         self.settings = settings
@@ -9,17 +10,25 @@ class AllSettings():
     def getAll(self):
         return self.settings
 
+    def __getitem__(self, name):
+        print("it works")
+        return self.get(name)
+
+    def __setitem__(self, name, val):
+        self.get(name).set(val)
+
     def get(self, name):
         for s in self.settings:
             if (s.name == name):
                 return s
+        print("No setting named '{}'".format(name))
         return None
 
     def takeArgs(self, args):
         for i in range(0, len(args)):
-            if (args[i].find("-") != -1 and settings.get(args[i].replace('-', '')) != None): # makes sure the args are valid settings
+            if (args[i].find("-") != -1 and self[args[i].replace('-', '')] != None): # makes sure the args are valid settings
                 if (i < len(args) - 1): # if there is another argument for the setting
-                    settings.get(args[i].replace('-', '')).setVal(args[i+1])
+                    self[args[i].replace('-', '')] = args[i+1]
 
 class Setting():
     def __init__(self, name, defautVal, possibleVals):
@@ -27,7 +36,7 @@ class Setting():
         self.name = name
         self.possibleVals = possibleVals
 
-    def setVal(self, newVal):
+    def set(self, newVal):
         if (newVal in self.possibleVals):
             self.value = newVal
         else:
