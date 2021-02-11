559f13d63b607b13f4482a24c6b00e0255bd81a0
Caden Churchman <ccaden21@gmail.com>
2021-02-11 16:57:50 -0600

    More code

--- external/test_google_appscript_server.js
+++ external/test_google_appscript_server.js
@@ -7,3 +7,38 @@ const doGet = (event = {}) => {
     // MailApp.sendEmail("ccaden21@gmail.com", "Hello", JSON.stringify(event))
     return ContentService.createTextOutput(output);
   }
+
+// This gets the main spreadsheet, use this function as it guarantees that the sheet is correct.
+// TODO: Maybe find a way to guarantee that this gets the right sheet without 'sheetName'
+function GetSheet() 
+{
+  var sheetName = "robot_server";
+  if ('sheet' in this) return this.sheet;
+  var test = SpreadsheetApp.getActiveSpreadsheet();
+  if (test.getName() == sheetName) {
+    this.sheet = test;
+    return this.sheet;
+  }
+  // else we will do more work to guarantee.
+  var ret = DriveApp.getFiles();
+  while (true) {
+    if (!ret.hasNext()) return None;
+    var file = ret.next();
+    if (file.getName() == sheetName) {
+      this.sheet = SpreadsheetApp.openById(file.getId());
+      return this.sheet;
+    }
+  }
+}
+
+// This gets a sheet with a name.
+function GetSheetWithName(name) 
+{
+  var sheet = GetSheet();
+  var ret = sheet.getSheetByName(name);
+  if (ret != null) {
+    return ret;
+  }
+  Logger.log("Invalid name: ", name);
+  throw ("Invalid sheet name: " + name);
+}
