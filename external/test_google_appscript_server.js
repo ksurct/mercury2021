f8e46c1b1eeb67279d8d03b8b25855163b47289e
Quen Guhr <qlg@ksu.edu>
2021-02-16 19:00:31 -0600

    Fixing bugs

--- external/test_google_appscript_server.js
+++ external/test_google_appscript_server.js
@@ -1,18 +1,34 @@
-
-
 const doGet = (event = {}) => {
     const { parameter } = event;
-    const { name = 'Anonymous', country = 'Unknown' } = parameter;
-    const output = `Hello ${name} from ${country}`;
-    // MailApp.sendEmail("ccaden21@gmail.com", "Hello", JSON.stringify(event))
-    return ContentService.createTextOutput(output);
+    const { blob = "" } = parameter;
+    var partsArray = blob.split(' ');
+    if(partsArray[0] == "computer"){
+      if(blob != ""){
+        log_blob_c(blob);
+      }
+    
+      read_blob_c();
+      return ContentService.createTextOutput(read_blob_c());
+      
+      return ContentService.createTextOutput("hi");
+    }
+    if(partsArray[0] == "robot"){
+      if(blob != ""){
+        log_blob_r(blob);
+      }
+    
+      read_blob_r();
+      return ContentService.createTextOutput(read_blob_r());
+      
+      return ContentService.createTextOutput("hi");
+    }
   }
 
 // This gets the main spreadsheet, use this function as it guarantees that the sheet is correct.
 // TODO: Maybe find a way to guarantee that this gets the right sheet without 'sheetName'
 function GetSheet() 
 {
-  var sheetName = "robot_server";
+  var sheetName = "QG Webserver";
   if ('sheet' in this) return this.sheet;
   var test = SpreadsheetApp.getActiveSpreadsheet();
   if (test.getName() == sheetName) {
@@ -42,3 +58,37 @@ function GetSheetWithName(name)
   Logger.log("Invalid name: ", name);
   throw ("Invalid sheet name: " + name);
 }
+
+function log_blob_c(string){
+  var write_to_sheet = GetSheetWithName("sheet1");
+  write_to_sheet.getRange(1,1).getCell(1,1).setValue(string) // This is the cell the computer writes too\
+
+}
+
+function read_blob_c(){
+  var read_sheet = GetSheetWithName("sheet1");
+  return read_sheet.getRange(2,1).getCell(1,1).getValue() //Cell 2B this would be the cell computer reads from.
+}
+
+function log_blob_r(string){
+  var write_to_sheet = GetSheetWithName("sheet1");
+  write_to_sheet.getRange(2,1).getCell(1,1).setValue(string) // This is the cell the robot writes too
+
+}
+
+function read_blob_r(){
+  var read_sheet = GetSheetWithName("sheet1");
+  return read_sheet.getRange(1,1).getCell(1,1).getValue() //This cell would be the cell robot would read too.
+}
+
+function test_write(){
+  log_blob_c("Hello")
+}
+
+function test_read(){
+  return ContentService.createTextOutput(read_blob_c())
+}
+
+
+
+
