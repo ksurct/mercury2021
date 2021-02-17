d8a9f3c929aed84276defaf3e05453ecc7be338c
Caden Churchman <ccaden21@gmail.com>
2021-02-16 19:20:04 -0600

    Added server code

--- mercury/playground_main.py
+++ mercury/playground_main.py
@@ -1,9 +1,20 @@
 
-
+import time
 import requests
 import time
-for i in range(0,1):
+
+from mercury.Webserver_Compter import webserver
+def send(string):
     start = time.time()
-    r = str(requests.get("https://script.google.com/macros/s/AKfycbzMSQjRxWl8jh1wGJHCdNgKcLCImKi5QqhUarJfdcwGahVCRU_NPeze/exec",{'name': "Quen"} ).content)
+    r = str(requests.get("https://script.google.com/macros/s/AKfycbyeRPqV1Z0vLX7ztAjCxlQ10JlUnFOmEw3ml2W7MuUaXyrOibkZSSDbjw/exec",{'blob': string} ).content)
     print (r)
     print("Took: ", time.time() - start)
+i = 0
+w = webserver()
+while (True):
+    # send("computer {}".format(i))
+    w.send("computer {}".format(i))
+    i = i + 1
+    # time.sleep(3)
+    
+
