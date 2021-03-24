6c0d0b3ae76e439b5c23dc5e1c16829ddac778ef
Alli Gladfelder <alligladfelder@gmail.com>
2021-03-24 18:02:10 -0500

    test for quitting if lost connection

--- mercury/real_main.py
+++ mercury/real_main.py
@@ -6,13 +6,20 @@ print("Running as actual robot")
 from mercury.robot_control.real_robot_control import RealRobotControl
 from mercury.internet_com import InternetCom
 from time import sleep
+import time
 com = InternetCom("robot", "http://10.150.251.154:8000/server")
 r = RealRobotControl()
 r.setClawServo(100)
+time_since_last_command = time.time()
 
 while(True):
+    if(time.time() - time_since_last_command > 2):
+        r.setAllMotorSpeeds(0)
     try:
-        r.runCommand(com.send("computer"))
+        command = com.send("computer")
+        if(command != ""):
+            time_since_last_command = time.time()
+        r.runCommand(command)
     except:
         print("We are experiencing technical difficulties.")
     sleep(0.25)
