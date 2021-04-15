367ccfcce4017765f579820912a61652f47f1817
Brandon Bednar <bwbednar@ksu.edu>
2021-04-15 12:49:26 -0500

    interface for sensor testing (work in prog)

--- mercury/driver_main.py
+++ mercury/driver_main.py
@@ -1,25 +1,79 @@
 print("Running as Driver")
 from mercury.robot_control.robot_control import RobotControl
 from mercury.internet_com import InternetCom
+#from mercury.driver_interface_main import CreateSensors
 import pyglet
 import pylint
 
 w = InternetCom("computer", "http://10.150.251.154:8000/server")
 
+
 window = pyglet.window.Window(width = 500, height = 500)
+
+"""
+#code in progress to streamline the label creation for sensors
+sensorsFL = pyglet.text.Label
+sensorsFM = pyglet.text.Label
+sensorsFR = pyglet.text.Label
+sensorsLT = pyglet.text.Label
+sensorsLB = pyglet.text.Label
+sensorsRT = pyglet.text.Label
+sensorsRB = pyglet.text.Label
+sensors = [sensorsFL, sensorsFM, sensorsFR, sensorsLT, sensorsLB, sensorsRT, sensorsRB]
+
+for s in sensors:
+    count = 7 
+    s = pyglet.text.Label('Sensor',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//4, y=window.height//8 * count,
+                          anchor_x='center', anchor_y='center')
+    count = count - 1"""
+#first iteration of working changing text for interface
+sensorsFL = pyglet.text.Label('Front Left',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 7,
+                          anchor_x='center', anchor_y='center')
+sensorsFM = pyglet.text.Label('Front middle',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 6,
+                          anchor_x='center', anchor_y='center')
+sensorsFR = pyglet.text.Label('Front right',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 5,
+                          anchor_x='center', anchor_y='center')
+sensorsLT = pyglet.text.Label('Left top',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 4,
+                          anchor_x='center', anchor_y='center')
+sensorsLB = pyglet.text.Label('Left bottom',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 3,
+                          anchor_x='center', anchor_y='center')
+sensorsRT = pyglet.text.Label('Right top',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8 * 2,
+                          anchor_x='center', anchor_y='center')
+sensorsRB = pyglet.text.Label('Right bottom',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//8,
+                          anchor_x='center', anchor_y='center')
+                          
+sensors = [sensorsFL, sensorsFM, sensorsFR, sensorsLT, sensorsLB, sensorsRT, sensorsRB]
+
 @window.event
 def on_draw():
     window.clear()
-    measures = sensors()
-    sensor1 = pyglet.text.Label("Work in Progress", 
-                          font_name ='Cooper', 
-                          font_size = 16, 
-                          x = 100,  
-                          y = 100, 
-                          anchor_x ='center',  
-                          anchor_y ='center')
-    #sensor2 = pyglet.text.Label("")
-    sensor1.draw()
+    for s in sensors:
+        s.draw()
+        
 
 @window.event
 def on_key_press(key, mod):
@@ -27,11 +81,13 @@ def on_key_press(key, mod):
     instruction = ""
     if(keyP == 'm'):
         instruction = r.createCommandInstruction(chr(key))
-        #w.send(instruction)
+        #response = w.send(instruction)
         pyglet.app.exit()
     else:
         instruction = r.createCommandInstruction(keyP)
-        #w.send(instruction)
+        #response = w.send(instruction)
+        for s in sensors:
+            s.text = keyP
 
 r = RobotControl()
 pyglet.app.run()
--- mercury/robot_control/sim_robot_control.py
+++ mercury/robot_control/sim_robot_control.py
@@ -5,6 +5,8 @@ import os
 from mercury.simulation.visualizer import Visualizer
 from mercury.simulation.ObstacleGenerator import ObstacleGenerator
 
+window = pyglet.window.Window(width = 500, height = 500)
+
 class SimRobotControl():
     def __init__(self, callback, rate, errorPercent):
         pyglet.clock.schedule_interval(callback, rate)
@@ -62,31 +64,26 @@ if (__name__ == "__main__"):
 
         #print out what the sensors are detecting for testing purposes
         measures = str(highRight[0]) + highRight[1] + "   " + str(lowRight[0]) + lowRight[1]+ "   " + str(highLeft[0]) + highLeft[1] + "   " + str(lowLeft[0]) + lowLeft[1] + "   " + str(frontRight[0]) + frontRight[1] + "   " + str(frontMid[0]) + frontMid[1] + "   " + str(frontLeft[0]) + frontLeft[1]
-        #print(measures)
         
         #If nothing is detected in front of the robot
         if (frontRight[0] == empty and frontLeft[0] == empty and frontMid[0] == empty):
             
             print("forward")
             r.move(10,0)
-            print(r.getProbableTheta())
         #something is in front a front facing sensors
         else: 
             #if there is nothing to the right, rotate right
             if (highRight[0] == empty and lowRight[0] == empty):
                 print("right")
                 r.rotate(PI/2, 0)
-                print(r.getProbableTheta())
             #if something is to the right, see if you can rotate left
             elif (highLeft[0] == empty and lowLeft[0] == empty):
                 print("left")
                 r.rotate(-PI/2,0)
-                print(r.getProbableTheta())
             #if there is something to both right and left, go backwards and then try to rotate 
             else:
                 print("reverse")
                 r.rotate(PI, 0)
-                print(r.getProbableTheta())
 
 
     r = SimRobotControl(callback, 0.1, 0)
